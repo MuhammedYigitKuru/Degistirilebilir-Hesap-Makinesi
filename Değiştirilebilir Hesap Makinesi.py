911a17aab284bd9d95fe58ec6e1a467f6484fdb7
MuhammedYigitKuru <muhammedyigitkuru@hotmail.com>
2026-01-14 17:40:40 +0300

    Update Değiştirilebilir Hesap Makinesi.py

--- Değiştirilebilir Hesap Makinesi.py
+++ Değiştirilebilir Hesap Makinesi.py
@@ -1,11 +1,1 @@
-import time
-def sum(a, b):
-    return (a + b)
 
-a = int(input('Sayı Giriniz:'))
-b = int(input('Sayı Giriniz:'))
-
-print(f'{a}+{b}={sum(a, b)}')
-time.sleep(1)
-print('Hesap Makinesini Denediğiniz İçin Teşekkür Ederiz!')
-time.sleep(4)
